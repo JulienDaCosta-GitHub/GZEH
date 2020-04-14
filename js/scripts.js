dba620010c1f0fd2a0412641e18291e30ab25aa8
JulienDaCosta-GitHub <55091161+JulienDaCosta-GitHub@users.noreply.github.com>
2020-04-14 19:12:36 +0200

    GLTF Loader
    First fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,59 @@
 var renderer	= new THREE.WebGLRenderer({
 	antialias	: true
 });
+
 /* Fullscreen */
+
 renderer.setSize( window.innerWidth, window.innerHeight );
+
 /* Append to HTML */
+
 document.body.appendChild( renderer.domElement );
 var onRenderFcts= [];
 var scene	= new THREE.Scene();
 var camera	= new THREE.PerspectiveCamera(25, window.innerWidth /    window.innerHeight, 0.01, 1000);
+
 /* Play around with camera positioning */
+
 camera.position.z = 15; 
 camera.position.y = 2;
+
 /* Fog provides depth to the landscape*/
+
 scene.fog = new THREE.Fog(0x000, 0, 45);
 ;(function(){
-	var light	= new THREE.AmbientLight( 0x202020 )
+	var light = new THREE.AmbientLight( 0x202020 )
 	scene.add( light )
-	var light	= new THREE.DirectionalLight('white', 5)
+	var light = new THREE.DirectionalLight('white', 5)
 	light.position.set(0.5, 0.0, 2)
 	scene.add( light )
-	var light	= new THREE.DirectionalLight('white', 0.75*2)
+	var light = new THREE.DirectionalLight('white', 0.75*2)
 	light.position.set(-0.5, -0.5, -2)
 	scene.add( light )		
 })()
-var heightMap	= THREEx.Terrain.allocateHeightMap(256,256)
+var heightMap = THREEx.Terrain.allocateHeightMap(256,256)
 THREEx.Terrain.simplexHeightMap(heightMap)	
-var geometry	= THREEx.Terrain.heightMapToPlaneGeometry(heightMap)
+var geometry = THREEx.Terrain.heightMapToPlaneGeometry(heightMap)
 THREEx.Terrain.heightMapToVertexColor(heightMap, geometry)
+
 /* Wireframe built-in color is white, no need to change that */
-var material	= new THREE.MeshBasicMaterial({
+
+var material = new THREE.MeshBasicMaterial({
 	wireframe: true
 });
-var mesh	= new THREE.Mesh( geometry, material );
+var mesh = new THREE.Mesh( geometry, material );
 scene.add( mesh );
 mesh.lookAt(new THREE.Vector3(0,1,0));
+
 /* Play around with the scaling */
-mesh.scale.y    = 8.5;
-mesh.scale.x    = 10.8;
-mesh.scale.z    = 0.08;
+
+mesh.scale.y = 8.5;
+mesh.scale.x = 10.8;
+mesh.scale.z = 0.08;
 mesh.scale.multiplyScalar(10);
+
 /* Play around with the camera */
+
 onRenderFcts.push(function(delta, now){
 	mesh.rotation.z += 0.0 * delta;	
 })
@@ -55,5 +69,15 @@ requestAnimationFrame(function animate(nowMsec){
 	onRenderFcts.forEach(function(onRenderFct){
 		onRenderFct(deltaMsec/1000, nowMsec/1000)
 	})
-})
+});
+
+var loader = new GLTFLoader();
+
+loader.load(
+	'src/scene.gltf',
+
+	function( gltf ) {
+		scene.add(  gltf.scene );
+	}
+)
 
