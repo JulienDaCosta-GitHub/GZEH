5622af8ebd919474ddc4bafd457fd7b4521f8842
JulienDaCosta-GitHub <55091161+JulienDaCosta-GitHub@users.noreply.github.com>
2020-05-12 20:27:34 +0200

    Nav + Animations
    Add a nav bar and texts with jquery animations

---
+++ js/animations.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $(".pyramid-gyro").mouseenter(function(){
+        $('.text1').css('opacity', '1');
+    })
+    $('.pyramid-gyro').mouseleave(function(){
+        $('.text1').css('opacity', '0');
+    })
+
+    $(".pyramid-gyro2").mouseenter(function(){
+        $('.text2').css('opacity', '1');
+    })
+    $('.pyramid-gyro2').mouseleave(function(){
+        $('.text2').css('opacity', '0');
+    })
+
+    $(".pyramid-gyro3").mouseenter(function(){
+        $('.text3').css('opacity', '1');
+    })
+    $('.pyramid-gyro3').mouseleave(function(){
+        $('.text3').css('opacity', '0');   
+    })
+
+    $(".pyramid-gyro4").mouseenter(function(){
+        $('.text4').css('opacity', '1');
+    })
+    $('.pyramid-gyro4').mouseleave(function(){
+        $('.text4').css('opacity', '0');   
+    })
+
+    $(".pyramid-gyro5").mouseenter(function(){
+        $('.text5').css('opacity', '1');
+    })
+    $('.pyramid-gyro5').mouseleave(function(){
+        $('.text5').css('opacity', '0');   
+    })
+
+    $(".pyramid-gyro6").mouseenter(function(){
+        $('.text6').css('opacity', '1');
+    })
+    $('.pyramid-gyro6').mouseleave(function(){
+        $('.text6').css('opacity', '0');   
+    })
+});
